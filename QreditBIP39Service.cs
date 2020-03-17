85202a6b954c8765e76971fb7830f713a77d8efb
Egor Tagirov <egortagirov@iMac-Egor.local>
2020-03-17 17:54:45 +0500

    init

---
+++ QreditBIP39Service.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using dotnetstandard_bip39;
+
+namespace Qredit
+{
+    public static class QreditBIP39Service
+    {
+        [FunctionName("GenerateRandomMnemonic")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+
+            BIP39 bip39 = new BIP39();
+
+            var dataMen = bip39.GenerateMnemonic(12, BIP39Wordlist.English);
+
+            return new OkObjectResult(dataMen);
+        }
+    }
+}
